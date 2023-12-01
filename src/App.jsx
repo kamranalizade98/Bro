47e21ea276c3629164cfb24b13eb063db5e4a8cb
kamranalizade98 <kamran.alizade98@gmail.com>
2023-12-01 17:43:01 +0400

    react tasks added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+      <Food></Food>
+    </>
+  );
+}
+
+export default App;
